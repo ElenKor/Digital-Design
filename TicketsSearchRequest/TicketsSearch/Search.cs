f6109197205753375f11fdd0a7d6c18b1f3375cf
ElKor <luisprostosuper@gmail.com>
2023-02-08 22:00:36 +0300

    HomeTask

---
+++ TicketsSearchRequest/TicketsSearch/Search.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using TicketsSearch.Models;
+using DocsVision.Platform.StorageServer.Extensibility;
+
+namespace TicketsSearch
+{
+    //класс серверного расширения
+    public class Search //:  StorageServerExtension
+    {
+        //метод, доступный на клиенте
+        //[ExtensionMethod]
+        public decimal MinimalTicketPrice(string destination, string dateFrom, string dateTo)
+        {
+            string departure_at = DateTime.Parse(dateFrom).ToString("yyyy-MM-dd"); //дата вылета из пункта отправления
+            string return_at = DateTime.Parse(dateTo).ToString("yyyy-MM-dd");//дата возвращения
+            string url = $"https://api.travelpayouts.com/aviasales/v3/prices_for_dates?origin=LED&destination={destination}&currency=rub&departure_at={departure_at}&return_at={return_at}&sorting=price&direct=true&limit=10&token=83f61e3efffda65096f35d90b7a24df7";
+            string result = getContent(url);
+            var tickets = JsonConvert.DeserializeObject<Ticket>(result);
+            var minimalTicketPrice = tickets.data
+                .Select(t => t.price)
+                .Min(); 
+            return minimalTicketPrice;
+        }
+
+        private static string getContent(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.Method = "GET";
+            request.Accept = "application/json";
+            request.UserAgent = "Mozilla/5.0 ....";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            StringBuilder output = new StringBuilder();
+            output.Append(reader.ReadToEnd());
+            response.Close();
+            return output.ToString();
+        }
+    }
+}
---
+++ TicketsSearchRequest/TicketsSearchRequest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketsSearchRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = "MOW";
+            string dateFrom = "01.12.2022";
+            string dateTo = "31.12.2022";
+
+            TicketsSearch.Search search = new TicketsSearch.Search();
+            decimal minPrice = search.MinimalTicketPrice(destination, dateFrom, dateTo);
+           
+            Console.WriteLine("Minimal ticket price from SPB to "+ destination+" = " + +minPrice + " rubles.");
+            //Console.ReadKey();
+        }
+    }
+}
